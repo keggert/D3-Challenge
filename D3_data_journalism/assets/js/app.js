3b847e685f029a8ab3a8353e37b1e21470c2d40f
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-08-25 13:44:06 -0500

    Here I go

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,21 @@
+// @TODO: YOUR CODE HERE!
+// D3 Journalism Data
+
+// Setting up Chart Parameters/Dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// SVG Margins
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+// Define dimensions of the Chart Area
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift latter by left and top margins
+var svg = d3.select(".")
