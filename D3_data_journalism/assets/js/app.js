4335a9c39fc1ca26ac39b98cc9783ced13631957
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-08-31 14:08:40 -0500

    Continuing on

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -48,8 +48,8 @@ function yScale(data, chosenYAxis) {
 };
 
 // Function for updating x-axis upon click on axis label
-function renderXAxis(newXscale, xAxis) {
-    var bottomAxis = d3.axisBottom(newXscale);
+function renderXAxis(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
 
     xAxis.transition()
         .duration(1000)
@@ -70,6 +70,91 @@ function renderYaxis(newYScale, yAxis) {
 };
 
 // Function used for updating circle group with transition to new circles
-fnction renderCircles(circlesGroup, newXscale, chosenXAxis) {
+function renderCircles(circlesGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXAxis]))
+        .attr("cy", d => newYScale(d[chosenYAxis]));
+
+    return circlesGroup;
+};
+
+// Function for updating the text group with a transition to the new text
+function renderText(textGroup, newXScale, chosenXAxis, newYScale, chosenYAxis) {
+    textGroup.transition()
+        .duration(1000)
+        .attr("x", d => newXScale(d[chosenXAxis]))
+        .attr("y", d => newYScale(d[chosenYAxis]))
+        .attr("text-anchor", "middle");
+
+    return textGroup;
+};
+
+// Function for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup, textGroup) {
+    if (chosenXAxis === "poverty") {
+        var xLabel = "In Poverty (%)";
+    }
+    else if (chosenXAxis === "age") {
+        var xLabel = "Age (Median)";
+    }
+    else {
+        var xLabel = "Household Income (Median)";
+    }
+    if (chosenYaxis === "obesity") {
+        var yLabel = "Obese (%)";
+    }
+    else if (chosenYAxis === "healthcare") {
+        var yLabel = "Lacks Healthcare (%)";
+    }
+    else {
+        var yLabel = "Smokes (%)";
+    }
+
+    var toolTip = d3.tip()
+        .attr("class", "tooltip d3-tip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`<strong>${d.abbr}</strong><br>${xLabel} ${d[chosenXAxis]}<br>${yLabel} ${d[chosenYAxis]}`);
+        });
     
+    // Create circles tooltip in the chart
+    circlesGroup.call(toolTip);
+
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+    })
+
+        // on mouseout event
+        .on("mouseout", function(data) {
+            toolTip.hide(data);
+        });
+
+    return circlesGroup;
 }
+
+// Retrieve data from CSV file
+d3.csv("assets/data/data.csv").then(function (data, err) {
+    if (err) throw err;
+
+    // Parse data
+    data.forEach(function(data) {
+        data.abbr = data.abbr;
+        data.sate = data.state;
+        data.poverty = +data.poverty;
+        data.age = +data.age;
+        data.income = +data.income;
+        data.obesity = +data.obesity;
+        data.healthcare = +data.healthcare;
+        data.smokes = +data.smokes;
+    });
+
+    // Create xLinearScale & yLinearScale functions for our chart
+    var xLinearScale = xScale(data, chosenXAxis);
+    var yLinearScale = yScale(data, chosenYAxis);
+
+    // Create axis functions for the chart
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+}
+)
