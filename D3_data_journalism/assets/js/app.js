38fe24810eec91730a67dd0df9f0dfb0005876f4
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-08-31 13:02:10 -0500

    Continuing on

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -19,7 +19,7 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift latter by left and top margins
 var svg = d3
-    .select("#chart")
+    .select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -32,4 +32,44 @@ var chartGroup = svg.append("g")
 var chosenXAxis = "poverty";
 var chosenYAxis = "obesity";
 
-// The function used for updating x-scale var upon click on axis label
+// The function used for updating x-scale and y-scale var upon click on axis label
+function xScale(data, chosenXAxis) {
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d[chosenXAxis] - 1), d3.max(data, d => d[chosenXAxis])])
+        .range([0, width]);
+    return xLinearScale;
+};
+
+function yScale(data, chosenYAxis) {
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[chosenYAxis])])
+        .range([height, 0]);
+    return yLinearScale;
+};
+
+// Function for updating x-axis upon click on axis label
+function renderXAxis(newXscale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXscale);
+
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+    
+    return xAxis;
+};
+
+// Function for updating y-axis upon click on axis label
+function renderYaxis(newYScale, yAxis) {
+    var leftAxis = d3.axisleft(newYScale);
+
+    yAxis.transition()
+        .duration(1000)
+        .call(leftAxis);
+
+    return yAxis;
+};
+
+// Function used for updating circle group with transition to new circles
+fnction renderCircles(circlesGroup, newXscale, chosenXAxis) {
+    
+}
